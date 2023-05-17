e8054bbd8956f850d2a60f39805aef81571215df
Gabriele Lopez <gabriele.ope@gmail.com>
2023-05-17 22:50:00 +0200

    Fixed margins and pointer

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -8,7 +8,7 @@ const Hamburger = (props: HamburgerProps) => {
       if (props.show) {
         return (
           <div className="bg-[#000]/[0.6] w-screen h-screen">
-            <div className="bg-[#5AA588] w-fit h-full shadow-[0_35px_60px_-15px_rgba(0,0,0,0.5)]">
+            <div className="bg-[#5AA588] w-fit h-full shadow-[0_35px_60px_-15px_rgba(0,0,0,0.5)] pt-9">
               <HamburgerElement name="Categories" source="/hamburgerIcons/category.svg" onClick={() => {}} />
               <HamburgerElement name="About us" source="/hamburgerIcons/info.svg" onClick={() => {}} />
               <HamburgerElement name="Themes" source="/hamburgerIcons/theme.svg" onClick={() => {}} />
@@ -21,7 +21,7 @@ const Hamburger = (props: HamburgerProps) => {
         )
       }
       return <></>;
-  //TODO log out
+      // TODO: Replace with transition
 };
 
 export default Hamburger;
--- src/components/HamburgerElement.tsx
+++ src/components/HamburgerElement.tsx
@@ -9,13 +9,13 @@ interface HamburgerElementProps {
 
 const HamburgerElement = ({ onClick, name, source, isLogout = false }: HamburgerElementProps) => {
   if (!isLogout) {
-    return <div className="flex items-center pl-4 pr-6 pt-12 first:pt-10" onClick={onClick}>
-      <Image width={40} height={40} src={source} alt={name} className="text-white cursor-pointer mr-3.5" ></Image>
+    return <div className="flex items-center ml-4 mr-6 mt-12 first:mt-0 cursor-pointer" onClick={onClick}>
+      <Image width={40} height={40} src={source} alt={name} className="text-white  mr-3.5" ></Image>
       <span className="text-white text-2xl">{name}</span>
     </div>;
   }
-  return <div className="flex items-center pl-4 pr-6 pt-40 first:pt-10" onClick={onClick}>
-    <Image width={40} height={40} src={source} alt={name} className="text-red-600 cursor-pointer mr-3.5"></Image>
+  return <div className="flex items-center ml-4 mr-6 mt-40 cursor-pointer" onClick={onClick}>
+    <Image width={40} height={40} src={source} alt={name} className="text-red-600 mr-3.5"></Image>
     <span className="text-red-600 text-2xl">{name}</span>
   </div>;
 }
