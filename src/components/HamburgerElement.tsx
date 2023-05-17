4912a82c03bf9178f32579d2b033fd9b6d65181e
Gabriele Lopez <gabriele.ope@gmail.com>
2023-05-17 22:43:32 +0200

    Changed onclick to correct element

--- src/components/HamburgerElement.tsx
+++ src/components/HamburgerElement.tsx
@@ -9,13 +9,13 @@ interface HamburgerElementProps {
 
 const HamburgerElement = ({ onClick, name, source, isLogout = false }: HamburgerElementProps) => {
   if (!isLogout) {
-    return <div className="flex items-center pl-4 pr-6 pt-12 first:pt-10">
-      <Image width={40} height={40} src={source} alt={name} className="text-white cursor-pointer mr-3.5" onClick={onClick}></Image>
+    return <div className="flex items-center pl-4 pr-6 pt-12 first:pt-10" onClick={onClick}>
+      <Image width={40} height={40} src={source} alt={name} className="text-white cursor-pointer mr-3.5" ></Image>
       <span className="text-white text-2xl">{name}</span>
     </div>;
   }
-  return <div className="flex items-center pl-4 pr-6 pt-40 first:pt-10">
-    <Image width={40} height={40} src={source} alt={name} className="text-red-600 cursor-pointer mr-3.5" onClick={onClick}></Image>
+  return <div className="flex items-center pl-4 pr-6 pt-40 first:pt-10" onClick={onClick}>
+    <Image width={40} height={40} src={source} alt={name} className="text-red-600 cursor-pointer mr-3.5"></Image>
     <span className="text-red-600 text-2xl">{name}</span>
   </div>;
 }
