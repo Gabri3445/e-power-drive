de794f67bb0c8757711119bae1353b0e4673cfb2
Gabriele Lopez <gabriele.ope@gmail.com>
2023-05-17 14:57:00 +0200

    Init

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,7 @@
+export default function LoginForm() {
+    return (
+        <div className="w-full h-screen bg-[#E2EEE2]">
+            <h1 className="">Login</h1>
+        </div>
+    )
+}
---
+++ src/components/QrCode.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { QrReader } from "react-qr-reader"
+import {useState} from "react"
+
+export default function qrScanner(){
+    const [scannedData, setScannedData] = useState<string | null>(null)
+    
+}
+
