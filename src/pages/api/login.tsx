f38c48cf7feca29ba37f5a4d6be43241e9bd2fe4
Gabriele Lopez <gabriele.ope@gmail.com>
2023-05-18 23:18:58 +0200

    Fixed wrong ID

--- src/pages/api/login.tsx
+++ src/pages/api/login.tsx
@@ -20,7 +20,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
             }
         })
         if (user) {
-            res.status(200).json({ sessionId: user.id });
+            res.status(200).json({ sessionId: user.sessionId });
             return;
         } else {
             res.status(404).end();
--- src/pages/api/register.tsx
+++ src/pages/api/register.tsx
@@ -26,7 +26,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
           password: request.password
         }
       });
-      const sessionId = newUser.id;
+      const sessionId = newUser.sessionId;
       return res.status(200).json({ sessionId });
     } 
     else {
